0d0d3b8ab47b47d497d226acb058d43012682285
Aislu Kharisova <arslanova1996ais@list.ru>
2024-04-27 16:03:00 +0300

    Седьмой commit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,15 @@
 import AboutProject from "./AboutProject/AboutProject";
 import "../Main/Main.css";
 import News from "./News/News";
-// import AboutUs from "./AboutUs/AboutUs";
-// import Newsletter from "./Newsletter/Newsletter";
+import AboutUs from "./AboutUs/AboutUs";
+import Newsletter from "./Newsletter/Newsletter";
 function Main({news,about, newsletter }) {
     return (
         <main className="main">
             <AboutProject/>
             <News ref={news}/>
-            {/*<AboutUs ref={about}/>*/}
-            {/*<Newsletter ref={newsletter}/>*/}
+            <AboutUs ref={about}/>
+            <Newsletter ref={newsletter}/>
         </main>
     )
 }
