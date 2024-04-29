0ef4697befc5a1f4266947433cba31e0a2d6b761
Aislu Kharisova <arslanova1996ais@list.ru>
2024-04-29 17:09:11 +0300

    одиннадцатый commit

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useFormValidation from "../../hooks/useFormValidation";
 import './Popup.css';
+import cross from "../../images/cross.svg";
 
 function Popup() {
 
@@ -19,16 +20,19 @@ function Popup() {
     return (
         <div className="popup popup_opened">
             <div className="popup__body">
-                <form className="popup__form" name="form__popup" onSubmit={handleSubmit}>
+                <div className="popup__group">
                     <h2 className="popup__title">СВЯЗАТЬСЯ С НАМИ</h2>
-                    <input name="text" className="newsletter__input" type="name" placeholder="Имя"
+                    <img src={cross} alt="Картинка крестика" className="popup__img"/>
+                </div>
+                <form className="popup__form" name="form__popup" onSubmit={handleSubmit}>
+                    <input name="text" className="popup__input" type="name" placeholder="Имя"
                            minLength="6" maxLength="40" onChange={handleChange} value={values.name || ''} required/>
                     <input type="text"
-                           className="newsletter__input"
+                           className="popup__input"
                            name="phone" placeholder="Телефон"
                            minLength="11"
                            maxLength="11" onChange={handleChange} value={values.phone || ''} required/>
-                    <div className="popup__group">
+                    <div className="popup__block">
                         <div className="popup__checkbox">
                             <label htmlFor="check">
                                 <input type="checkbox" id="check"/>
